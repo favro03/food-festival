1fa792c047f4db81b1e573b18706327601a7fbef
tfavro <wetr9902@gmail.com>
2022-04-01 11:10:51 -0500

    PWA

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -33,9 +33,9 @@ eval("/*!\n  * Bootstrap v4.3.1 (https://getbootstrap.com/)\n  * Copyright 2011-
 /*!***********************************!*\
   !*** ./assets/img/food-table.jpg ***!
   \***********************************/
-/***/ ((module) => {
+/***/ (() => {
 
-eval("module.exports = \"assets/img/food-table.jpg\";\n\n//# sourceURL=webpack://food-festival/./assets/img/food-table.jpg?");
+eval("throw new Error(\"Module build failed (from ./node_modules/image-webpack-loader/index.js):\\nTypeError: Cannot read properties of undefined (reading 'imageWebpackLoader')\\n    at Object.exports.getLoaderConfig (/Users/tracyfavro/Desktop/projects/food-festival/node_modules/image-webpack-loader/node_modules/loader-utils/index.js:124:37)\\n    at Object.module.exports (/Users/tracyfavro/Desktop/projects/food-festival/node_modules/image-webpack-loader/index.js:13:28)\");\n\n//# sourceURL=webpack://food-festival/./assets/img/food-table.jpg?");
 
 /***/ }),
 
@@ -43,9 +43,9 @@ eval("module.exports = \"assets/img/food-table.jpg\";\n\n//# sourceURL=webpack:/
 /*!******************************!*\
   !*** ./assets/img/grill.jpg ***!
   \******************************/
-/***/ ((module) => {
+/***/ (() => {
 
-eval("module.exports = \"assets/img/grill.jpg\";\n\n//# sourceURL=webpack://food-festival/./assets/img/grill.jpg?");
+eval("throw new Error(\"Module build failed (from ./node_modules/image-webpack-loader/index.js):\\nTypeError: Cannot read properties of undefined (reading 'imageWebpackLoader')\\n    at Object.exports.getLoaderConfig (/Users/tracyfavro/Desktop/projects/food-festival/node_modules/image-webpack-loader/node_modules/loader-utils/index.js:124:37)\\n    at Object.module.exports (/Users/tracyfavro/Desktop/projects/food-festival/node_modules/image-webpack-loader/index.js:13:28)\");\n\n//# sourceURL=webpack://food-festival/./assets/img/grill.jpg?");
 
 /***/ }),
 
